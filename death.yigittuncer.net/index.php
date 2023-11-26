dfa3f486507c97b96d088590f379c3db3e7aee72
yigittuncer07 <yigittuncer@marun.edu.tr>
2023-11-26 04:19:22 +0300

    Added new subdomain, death.yigittuncer.net

---
+++ death.yigittuncer.net/index.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>death.yigittuncer.net</title>
+    <link rel="icon" href="content/favicon.png">
+    <link rel="stylesheet" href="content/style.css">
+</head>
+
+<body>
+    <main>
+
+        <a href="http://yigittuncer.net">
+            <header>
+                <h1 id="mainHeader">DEATH</h1>
+            </header>
+        </a>
+
+        <h2>UNDER DEVELOPMENT:D</h2>
+
+    </main>
+</body>
+
+</html>
